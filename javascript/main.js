9eeb42c06043f334de894b4138ba9365e8bc6245
Stephen Sylvia <ssylvia@esri.com>
2012-11-29 13:54:16 -0500

    initial setup

---
+++ javascript/main.js
@@ -0,0 +1,5 @@
+//Add Additional Esri API Scripts
+dojo.require("esri.map");
+dojo.require("dijit.layout.BorderContainer");
+dojo.require("dijit.layout.ContentPane");
+dojo.require("esri.arcgis.utils");
