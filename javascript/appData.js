4ec26d792c8363edc79e2b36cf928a13d81477c6
Stephen Sylvia <ssylvia@esri.com>
2012-11-29 17:39:37 -0500

    basic framework

---
+++ javascript/appData.js
@@ -0,0 +1,7 @@
+window.appData = {
+
+  "webmap" : "5ae9e138a17842688b0b79283a4353f6",
+  "title" : "Millions of bats are dying from white nose syndrome",
+  "subtitle" : "As the disease rapidly spreads across North America, more and more populations are threatened."
+
+};
--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,45 @@
 //Add Additional Esri API Scripts
 dojo.require("esri.map");
+dojo.require("esri.arcgis.utils");
+dojo.require("dijit.dijit");
 dojo.require("dijit.layout.BorderContainer");
 dojo.require("dijit.layout.ContentPane");
-dojo.require("esri.arcgis.utils");
+
+$(document).ready(function(){
+    $("#title").html(appData.title);
+    $("#subtitle").html(appData.subtitle);
+});
+
+dojo.addOnLoad(function(){
+  createMap();
+});
+
+var createMap = function(){
+  var mapDeferred = esri.arcgis.utils.createMap(appData.webmap, "map", {
+    mapOptions: {
+      extent : new esri.geometry.Extent({"xmin":-15291242.010989934,"ymin":540184.1574534695,"xmax":-6192178.163924971,"ymax":8602150.404745435,"spatialReference":{"wkid":102100}}),
+      slider : true,
+      sliderStyle : "small",
+      nav : false
+    }
+  });
+  mapDeferred.then(function(response) {
+    window.map = response.map;
+
+    //resize the map when the browser resizes
+    dojo.connect(dijit.byId('map'), 'resize', map,map.resize);
+
+    //add the legend
+    var layers = response.itemInfo.itemData.operationalLayers;
+    if(map.loaded){
+      //initMap(layers);
+    }
+    else{
+      dojo.connect(map,"onLoad",function(){
+        //initMap(layers);
+      });
+    }
+  },function(error){
+    console.log("Map creation failed: ", dojo.toJson(error));
+  });
+};
